be7dc8c28435bc64fa8a76e7b8c3abeb07ac3191
fabiourj <fabiourj@gmail.com>
2023-07-29 06:48:10 -0300

    classe Adconfig.java

---
+++ app/src/main/java/com/sherdle/universal/AdConfig.java
@@ -0,0 +1,96 @@
+
+
+package com.sherdle.universal;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.AsyncTask;
+
+
+public class AdConfig {
+    private static AdConfig instance;
+
+    private String bannerAd;
+    private String interstitialAd;
+    private Map<String, String> admobIds;
+    private Map<String, String> applovinMaxIds;
+    private Map<String, String> adcolonyIds;
+    private String ironsourceAppId;
+
+    private AdConfig() {
+    }
+
+    public static AdConfig getInstance() {
+        if (instance == null) {
+            instance = new AdConfig();
+        }
+        return instance;
+    }
+
+    // Adicione getters e setters para cada campo aqui
+    // ...
+
+    public void initializeAds(Context context) {
+        // Aqui você pode adicionar o código para inicializar as redes de anúncios
+        // Você pode usar os getters para obter as informações da rede de anúncios
+        // Por exemplo:
+        switch (getBannerAd()) {
+            case "Admob":
+                // Add initialization code for Admob banner ad
+                break;
+            case "Applovin MAX":
+                // Add initialization code for Applovin MAX banner ad
+                break;
+            case "Adcolony":
+                // Add initialization code for Adcolony banner ad
+                break;
+            case "Ironsource":
+                // Add initialization code for Ironsource banner ad
+                break;
+        }
+        // ...
+    }
+
+    public void showBannerAd(AdView adView) {
+        // Aqui você pode adicionar o código para exibir os anúncios de banner
+        // Você pode usar os getters para obter as informações da rede de anúncios
+        // Por exemplo:
+        switch (getBannerAd()) {
+            case "Admob":
+                // Add code to show Admob banner ad
+                break;
+            case "Applovin MAX":
+                // Add code to show Applovin MAX banner ad
+                break;
+            case "Adcolony":
+                // Add code to show Adcolony banner ad
+                break;
+            case "Ironsource":
+                // Add code to show Ironsource banner ad
+                break;
+        }
+        // ...
+    }
+
+    public void showInterstitialAd(InterstitialAd interstitialAd) {
+        // Aqui você pode adicionar o código para exibir os anúncios intersticiais
+        // Você pode usar os getters para obter as informações da rede de anúncios
+        // Por exemplo:
+        switch (getInterstitialAd()) {
+            case "Admob":
+                // Add code to show Admob interstitial ad
+                break;
+            case "Applovin MAX":
+                // Add code to show Applovin MAX interstitial ad
+                break;
+            case "Adcolony":
+                // Add code to show Adcolony interstitial ad
+                break;
+            case "Ironsource":
+                // Add code to show Ironsource interstitial ad
+                break;
+        }
+        // ...
+    }
+}
