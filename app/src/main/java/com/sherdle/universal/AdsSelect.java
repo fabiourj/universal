26eaa4509356c8cbb9bf137c9f3493bc819ef691
fabiourj <fabiourj@gmail.com>
2023-07-30 14:04:04 -0300

    AdsSelect

--- app/src/main/java/com/sherdle/universal/AdsSelect.java
+++ app/src/main/java/com/sherdle/universal/AdsSelect.java
@@ -5,56 +5,95 @@ package com.sherdle.universal;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
-import android.os.AsyncTask;
-import org.json.JSONObject;
 import java.util.Map;
-
+import android.content.Context;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import android.os.AsyncTask;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class AdsSelect {
     private static AdsSelect instance;
-    private JSONObject AdsSelect;
-    private String bannerAds;
-    private String interstitialAds;
-   
-    private String applovinMaxIds;
-    private String adcolonyIds;
-    private String IronsourceAppId;
+    private JSONObject adsConfig;
+    private String mainBannerAds;
+    private String mainInterstitialAds;
+    private String adcolonyAppId;
+    private String adcolonyBannerZoneId;
+    private String adcolonyInterstitialZoneId;
+    private String ironsourceAppId;
 
-//////////////////////////////////////////////////////////////////////////////
+    private AdsSelect(Context context) {
+        try {
+            // Carregar o arquivo de configuração de anúncios ads.json
+            InputStream is = context.getAssets().open("ads.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            String json = new String(buffer, StandardCharsets.UTF_8);
+            JSONArray jsonArray = new JSONArray(json);
+            adsConfig = jsonArray.getJSONObject(0);
 
-
-///////////////////////////////////////////////////////////////////////////////    
-
-   private AdsSelect(Context context) {
-        // Carregar o arquivo de configuração de anúncios ads.json
-      
-        
+            // Extrair as informações da rede de anúncios
+            mainBannerAds = adsConfig.getString("main_bannerAds");
+            mainInterstitialAds = adsConfig.getString("main_interstitialAds");
+            adcolonyAppId = adsConfig.getString("Adcolony_appId");
+            adcolonyBannerZoneId = adsConfig.getString("Adcolony_bannerZoneId");
+            adcolonyInterstitialZoneId = adsConfig.getString("Adcolony_interstitialZoneId");
+            ironsourceAppId = adsConfig.getString("Ironsource_appId");
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-public static synchronized AdsSelect getInstance(Context context) {
-    if (instance == null) {
-        instance = new AdsSelect(context.getApplicationContext());
-    }
-    return instance;
-}
-
-
- public static synchronized AdsSelect getInstance(Context context) {
+    public static synchronized AdsSelect getInstance(Context context) {
         if (instance == null) {
-            instance = new AdsSelect(context);
+            instance = new AdsSelect(context.getApplicationContext());
         }
         return instance;
     }
-   
+
+    // Getters para as informações da rede de anúncios
+    public String getMainBannerAds() {
+        return mainBannerAds;
+    }
+
+    public String getMainInterstitialAds() {
+        return mainInterstitialAds;
+    }
+
+    public String getAdcolonyAppId() {
+        return adcolonyAppId;
+    }
+
+    public String getAdcolonyBannerZoneId() {
+        return adcolonyBannerZoneId;
+    }
+
+    public String getAdcolonyInterstitialZoneId() {
+        return adcolonyInterstitialZoneId;
+    }
+
+    public String getIronsourceAppId() {
+        return ironsourceAppId;
+    }
+
+    // Métodos para inicializar e exibir anúncios
+    // TODO: Adicione o código para inicializar e exibir anúncios
+
+
 
 
     
 
-    public void initializeAds(Context context) {
+    public void initialize_bannerAds(Context context) {
         // Aqui você pode adicionar o código para inicializar as redes de anúncios
         // Você pode usar os getters para obter as informações da rede de anúncios
         // Por exemplo:
-        switch (getBannerAds()) {
+        switch (main_bannerAds()) {
            
             case "Applovin MAX":
                 // Add initialization code for Applovin MAX banner ad
@@ -73,7 +112,7 @@ public static synchronized AdsSelect getInstance(Context context) {
         // Aqui você pode adicionar o código para exibir os anúncios de banner
         // Você pode usar os getters para obter as informações da rede de anúncios
         // Por exemplo:
-        switch (getBannerAds()) {
+        switch (main_bannerAds()) {
            
             case "Applovin MAX":
                 // Add code to show Applovin MAX banner ad
@@ -88,11 +127,30 @@ public static synchronized AdsSelect getInstance(Context context) {
         // ...
     }
 
-    public void showInterstitialAds() {
+     public void initialize_InterstitialAds(Context context) {
+        // Aqui você pode adicionar o código para inicializar as redes de anúncios
+        // Você pode usar os getters para obter as informações da rede de anúncios
+        // Por exemplo:
+        switch (main_interstitialAds()) {
+           
+            case "Applovin MAX":
+                // Add initialization code for Applovin MAX banner ad
+                break;
+            case "Adcolony":
+                // Add initialization code for Adcolony banner ad
+                break;
+            case "Ironsource":
+                // Add initialization code for Ironsource banner ad
+                break;
+        }
+        // ...
+    }
+
+    public void show_InterstitialAds() {
         // Aqui você pode adicionar o código para exibir os anúncios intersticiais
         // Você pode usar os getters para obter as informações da rede de anúncios
         // Por exemplo:
-        switch (getInterstitialAds()) {
+        switch (main_interstitialAds()) {
             
             case "Applovin MAX":
                 // Add code to show Applovin MAX interstitial ad
@@ -104,33 +162,11 @@ public static synchronized AdsSelect getInstance(Context context) {
                 // Add code to show Ironsource interstitial ad
                 break;
         }
-        // ...
+        
+
+
     }
 
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-public void setBannerAds(String bannerAds) {  
-    this.bannerAds = bannerAds;
-}
-
-public void setInterstitialAds(String interstitialAds) {
-    this.interstitialAds = interstitialAds;
-}
-
-
-
-public void setAdcolonyIds(String adcolonyIds) {
-    this.adcolonyIds = adcolonyIds;
-}
-
-public void setIronsourceAppId(String setIronsourceAppId) {
-    this.setIronsourceAppId = IronsourceAppId;
-}
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 }
